588e24ec6b12c8353ad177fe700d4caf0d28eb4a
sergnsn <s.novikov@inpglobal.com>
2019-09-28 00:08:05 +0300

    флэйквосем больше не ругается...

--- yachts.py
+++ yachts.py
@@ -1,8 +1,9 @@
 
-from graph import *
-from math import *
+from graph import brushColor, circle, penColor, penSize, moveTo, lineTo, rectangle, polygon, canvasSize, run
+from math import sin, cos, pi, sqrt
 
-def cloud(x, y, r): # центр левого верхнего круга, радиусы
+
+def cloud(x, y, r):   # center of the upper left circle, rads
     brushColor('white')
     circle(x, y, r)
     circle(x + 1.5*r, y, r)
@@ -12,87 +13,97 @@ def cloud(x, y, r): # центр левого верхнего круга, ра�
     circle(x + 3*r, y, r)
     circle(x + 3.5*r, y + r, r)
 
-def umbrella(x, y, h): # куда воткнули ножку, длина ножки
+
+def umbrella(x, y, h):   # where to put the leg, leg length
     penColor(170, 102, 0)
     penSize(h/20)
     moveTo(x, y)
-    lineTo(x, y - h) 
+    lineTo(x, y - h)
     penSize(0)
     brushColor(255, 88, 118)
     polygon([(x + h/40, y - h), (x - h/40, y - h), (x - h/2, y - 5/6*h), (x + h/2, y - 5/6*h)])
     penSize(3)
     penColor('brown')
-    for i in range(3): # полоски на зонте (2 цикла)
+    for i in range(3):   # lines on the umbrella
         moveTo(x - h/40, y - h)
         lineTo(x - h/2 + (i+1) * 0.15*h, y - 5/6*h)
     for i in range(3):
         moveTo(x + h/40, y - h)
         lineTo(x + h/2 - (i+1) * 0.15*h, y - 5/6*h)
 
-def yacht(x, y, s): # x, y основание мачты, s  -  размер (1>2>...)
-    mast = 230/s # высота мачты
-    thi = 60/s # толщина яхты
-    bow = 100/s # длина носа
-    col = 5/s # толщина столба мачты
-    # мачта
+
+def yacht(x, y, s):   # mast base coordinates, s  -  size (1>2>...)
+    mast = 230/s   # mast high
+    thi = 60/s   # yacht thickness
+    bow = 100/s   # bow length
+    col = 5/s   # mast column thickness
+    # mast
     penSize(thi/5)
     penColor('black')
     moveTo(x, y)
-    lineTo(x, y - mast) 
-    # парус
-    penSize(thi/15)
+    lineTo(x, y - mast)
+    # sail
     penColor('white')
     brushColor(230, 230, 250)
     polygon([(x + col, y - mast), (x + col + bow, y - mast/2), (x + col + bow/4, y - mast/2)])
     polygon([(x + col, y), (x + col + bow, y - mast/2), (x + col + bow/4, y - mast/2)])
-    # корпус
+    # boat
     brushColor(170, 102, 0)
     penColor(170, 102, 0)
     rectangle(x - mast/2, y, x + mast/2, y + thi)
     polygon([(x + mast/2, y), (x + mast/2 + bow, y), (x + mast/2, y + thi)])
-    for i in range(100): 
+    for i in range(100):
         phi1 = i * pi / 200
         phi2 = (i+1) * pi / 200
-        polygon([(x - mast/2  -  thi*cos(phi1), y  +  thi*sin(phi1)), (x - mast/2  -  thi*cos(phi2), y  +  thi*sin(phi2)), (x - mast/2, y)])
-    # окно
+        polygon([(x - mast/2 - thi*cos(phi1), y + thi*sin(phi1)), (x - mast/2 - thi*cos(phi2), y + thi*sin(phi2)),
+                                                                  (x - mast/2, y)])
+    # window
     penSize(0)
     brushColor('black')
     circle(x + mast/2 + bow/4, y + thi/3, thi/4)
     brushColor('white')
     circle(x + mast/2 + bow/4, y + thi/3, thi/6)
 
-def sun(xc, yc, r, ray): # центр, радиус, длина луча  +  радиус
+
+def sun(x, y, r, ray):  # center coordinates, rad, ray length + rad
     brushColor('yellow')
     penColor('yellow')
     penSize(0)
-    v = 40 # кол-во лучей
+    v = 40   # number of rays
     for i in range(v):
         phi1 = 2 * i * pi / v
         phi2 = 2 * (i+1) * pi / v
-        polygon ([(xc, yc), (xc  +  r*cos(phi1), yc  +  r*sin(phi1)), (xc  +  ray*cos(phi1 + pi/v), yc  +  ray*sin(phi1 + pi/v)), (xc  +  r*cos(phi2), yc  +  r*sin(phi2))])
+        polygon([(x, y), (x + r*cos(phi1), y + r*sin(phi1)),
+                         (x + ray*cos(phi1 + pi/v), y + ray*sin(phi1 + pi/v)), (x + r*cos(phi2), y + r*sin(phi2))])
 
-def waves(y): # y-координата границы моря с песком
+
+def waves(y):   # y coordinate of bother between sea and sand
     penSize(0)
     r = 40
     n = 15
     for i in range(n):
         x = i * r * 2 * sqrt(3)
         brushColor(0, 0, 205)
-        circle(x - r*sqrt(3)/2, y - r/2, r) 
+        circle(x - r*sqrt(3)/2, y - r/2, r)
         brushColor(255, 229, 124)
-        circle(x + r*sqrt(3)/2, y + r/2, r) 
+        circle(x + r*sqrt(3)/2, y + r/2, r)
 
-# фон
-canvasSize(1000, 600)
-brushColor(135, 206, 250)
-rectangle(0, 0, 1000, 300)
-brushColor(255, 229, 124)
-rectangle(0, 600, 1000, 500)
-brushColor(0, 0, 205)
-rectangle(0, 300, 1000, 500)
+
+def view():
+    # sky
+    canvasSize(1000, 600)
+    brushColor(135, 206, 250)
+    rectangle(0, 0, 1000, 300)
+    # sand
+    brushColor(255, 229, 124)
+    rectangle(0, 600, 1000, 500)
+    # sea
+    brushColor(0, 0, 205)  # море
+    rectangle(0, 300, 1000, 500)
+
+
+view()
 waves(500)
-
-# предметы
 sun(850, 100, 55, 80)
 cloud(110, 65, 22)
 cloud(470, 60, 42)
@@ -103,5 +114,3 @@ yacht(750, 390, 1)
 yacht(440, 325, 2)
 
 run()
-
-
